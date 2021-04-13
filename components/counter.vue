b749a344aa728eb514b4e0f838e2655d8d5dd87d
raishin7773 <raishin7773@yahoo.co.jp>
2021-04-13 23:41:08 +0900

    カウンター機能の実装

---
+++ components/counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h2>{{title}}</h2>
+    counter:{{$store.state.counter.counter}}
+    <button v-on:click="$store.commit('counter/countUp')">+1</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "counter.vue",
+  data: function () {
+    return {
+      title:'counter app'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
